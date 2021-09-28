20efada6f7823862c88329a0fe017552b1e39134
Creepler13 <denis.schlichting03@gmail.com>
2021-09-28 20:55:06 +0200

    update

--- src/gui/ImageEditor.java
+++ src/gui/ImageEditor.java
@@ -10,36 +10,35 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
-import javax.swing.plaf.DimensionUIResource;
 
+import core.filter.FilterLib;
 import core.filter.filterTypes.Filter;
 import core.filter.filters.GameOfLife;
 import de.informatics4kids.Picture;
 
-public class Viewer implements ActionListener {
+public class ImageEditor {
 
-	private BufferedImage image;
-	private Picture picture;// the rasterized image
-	private JFrame frame; // on-screen view
-	private String filename; // name of file
+	public BufferedImage image;
+	public Picture picture;
+	private String filename;
+	private Filter[] filter;
 
+	private JFrame frame;
+	private JMenuBar menuBar = new JMenuBar();
 	private JBackgroundPanel jbc = new JBackgroundPanel();
 
 	/**
 	 * Create a picture by reading in a .png, .gif, or .jpg from the given filename
 	 * or URL name.
 	 */
-	public Viewer(Picture image) {
-		show();
+	public ImageEditor(Picture image) {
 		setImage(image, true);
 	}
 
@@ -59,14 +58,8 @@ public class Viewer implements ActionListener {
 		}
 	}
 
-	/**
-	 * Zeigt das Bild innerhalb einen Fensters an. Das Bild kann als .png oder .jpg
-	 * Bild gespeichert werden.
-	 */
-
-	private JMenuBar menuBar = new JMenuBar();
-
-	public void show() {
+	@SuppressWarnings("deprecation")
+	public void openGui() {
 
 		// create the GUI for viewing the image if needed
 		if (frame == null) {
@@ -74,91 +67,99 @@ public class Viewer implements ActionListener {
 
 			JMenu menu = new JMenu("File");
 			menuBar.add(menu);
-			JMenuItem menuItemSave = new JMenuItem(" Save...   ");
-			menuItemSave.addActionListener(this);
+			JMenuItem menuItemSave = new JMenuItem("Save...");
+			menuItemSave.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					FileDialog chooser = new FileDialog(frame, "Dateiendung: .png oder .jpg", FileDialog.SAVE);
+					chooser.setVisible(true);
+					if (chooser.getFile() != null) {
+						save(chooser.getDirectory() + File.separator + chooser.getFile());
+					}
+				}
+			});
 			menuItemSave.setAccelerator(
 					KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
-			menu.add(menuItemSave);
 
-			JMenuItem menuItemOpen = new JMenuItem(" Open...   ");
+			JMenuItem menuItemOpen = new JMenuItem("Open...");
 			menuItemOpen.addActionListener(new ActionListener() {
-
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					JFileChooser jfc = new JFileChooser();
 					jfc.showOpenDialog(null);
 					File f = jfc.getSelectedFile();
-
-					if (!(f.getAbsolutePath().toLowerCase().endsWith("jpg")
-							|| f.getAbsolutePath().toLowerCase().endsWith("png")
-							|| f.getAbsolutePath().toLowerCase().endsWith("jpeg"))) {
-						JOptionPane.showMessageDialog(null, "The selected file is is not of the right type (jpg,png)",
-								"FileType Error", JOptionPane.ERROR_MESSAGE);
-						return;
-					}
-
-					Picture p = new Picture(f.getAbsolutePath());
-					setImage(p, true);
-
+					open(f);
 				}
-
 			});
+
 			menu.add(menuItemOpen);
+			menu.add(menuItemSave);
+
+			addFilter();
+			setImage(picture, true);
 
 			frame.setJMenuBar(menuBar);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setTitle("Original");
-			frame.setResizable(false);
-			// frame.pack();
+			frame.setResizable(true);
 			frame.setVisible(true);
 		}
 
 		// frame.repaint(/* draw */);
 	}
 
-	private void save(String name) {
+	public void save(String name) {
 		save(new File(name));
+	}
+
+	public void open(String name) {
+		open(new File(name));
+	}
+
+	public void open(File file) {
+		if (!(file.getAbsolutePath().toLowerCase().endsWith("jpg")
+				|| file.getAbsolutePath().toLowerCase().endsWith("png")
+				|| file.getAbsolutePath().toLowerCase().endsWith("jpeg"))) {
+			if (frame != null) {
+				JOptionPane.showMessageDialog(null, "The selected file is is not of the right type (jpg,png)",
+						"FileType Error", JOptionPane.ERROR_MESSAGE);
+			} else {
+				System.err.println("FileType Error - The selected file is is not of the right type (jpg,png)");
+
+			}
+			return;
+		}
+
+		Picture p = new Picture(file.getAbsolutePath());
+		setImage(p, true);
+
 	}
 
 	private static Filter lastApplied;
 	private static int lastAppliedCount = 1;
 
-	public void addFilter(Filter[] filter) {
+	@SuppressWarnings("deprecation")
+	private void addFilter() {
+		filter = new Filter[FilterLib.class.getDeclaredFields().length];
+
+		for (int i = 0; i < filter.length; i++) {
+			try {
+				filter[i] = (Filter) FilterLib.class.getDeclaredFields()[i].get(null);
+			} catch (IllegalArgumentException | IllegalAccessException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
+
 		JMenu menu = new JMenu("Filter");
 		menuBar.add(menu);
 
 		for (Filter f : filter) {
 			JMenuItem menuItem = new JMenuItem(f.getClass().getSimpleName());
 			menuItem.addActionListener(new ActionListener() {
-
 				@Override
 				public void actionPerformed(ActionEvent e) {
-
-				//	f.timed();
-					setImage(f.apply(picture), false);
-
-					if (lastApplied != null)
-						if (f == lastApplied) {
-							lastAppliedCount++;
-
-							String title = "";
-							String[] titleSplit = frame.getTitle().split(" ");
-							for (int i = 0; i < titleSplit.length - (lastAppliedCount == 2 ? 0 : 1); i++) {
-								title += titleSplit[i] + " ";
-							}
-
-							frame.setTitle(title + "x" + lastAppliedCount);
-							lastApplied = f;
-
-							return;
-						}
-
-					frame.setTitle(frame.getTitle() + " -> " + f.getClass().getSimpleName());
-					lastApplied = f;
-					lastAppliedCount = 1;
-
+					applyFilter(f);
 				}
-
 			});
 
 			if (f.getClass() == GameOfLife.class)
@@ -170,10 +171,38 @@ public class Viewer implements ActionListener {
 
 	}
 
+	public void applyFilter(Filter filter) {
+
+		setImage(filter.apply(picture), false);
+
+		if (frame != null) {
+			if (lastApplied != null)
+				if (filter == lastApplied) {
+					lastAppliedCount++;
+
+					String title = "";
+					String[] titleSplit = frame.getTitle().split(" ");
+					for (int i = 0; i < titleSplit.length - (lastAppliedCount == 2 ? 0 : 1); i++) {
+						title += titleSplit[i] + " ";
+					}
+
+					frame.setTitle(title + "x" + lastAppliedCount);
+					lastApplied = filter;
+
+					return;
+				}
+
+			frame.setTitle(frame.getTitle() + " -> " + filter.getClass().getSimpleName());
+			lastApplied = filter;
+			lastAppliedCount = 1;
+
+		}
+	}
+
 	/**
 	 * Save the picture to a file in a standard image format.
 	 */
-	private void save(File file) {
+	public void save(File file) {
 		this.filename = file.getName();
 		if (frame != null) {
 			frame.setTitle(filename);
@@ -195,12 +224,5 @@ public class Viewer implements ActionListener {
 	 * Opens a save dialog box when the user selects "Save As" from the menu. This
 	 * method should not be called programmatically.
 	 */
-	public void actionPerformed(ActionEvent e) {
-		FileDialog chooser = new FileDialog(frame, "Dateiendung: .png oder .jpg", FileDialog.SAVE);
-		chooser.setVisible(true);
-		if (chooser.getFile() != null) {
-			save(chooser.getDirectory() + File.separator + chooser.getFile());
-		}
-	}
 
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,27 +1,17 @@
 package gui;
 
-import core.filter.FilterLib;
-import core.filter.filterTypes.Filter;
 import de.informatics4kids.Picture;
 
 public class Main {
 
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
 
-		for (int i = 0; i < filter.length; i++) {
-			filter[i] = (Filter) FilterLib.class.getDeclaredFields()[i].get(null);
-		}
-
 		Picture p = new Picture("src/lib/Wiener_filter_-_my_dog.JPG");
 
-		Viewer viewerO = new Viewer(p);
+		ImageEditor viewerO = new ImageEditor(p);
 
-		viewerO.addFilter(filter);
-
-		viewerO.show();
+		viewerO.openGui();
 
 	}
-
-	private static Filter[] filter = new Filter[FilterLib.class.getDeclaredFields().length];
 
 }
